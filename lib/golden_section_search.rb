75a8a64f60f936d21066ff18f6eaab707a03479c
Wendel Scardua <wendelscardua@gmail.com>
2021-10-20 20:16:02 -0300

    First commit (bundle gem)

---
+++ lib/golden_section_search.rb
@@ -0,0 +1,6 @@
+require "golden_section_search/version"
+
+module GoldenSectionSearch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/golden_section_search/version.rb
@@ -0,0 +1,3 @@
+module GoldenSectionSearch
+  VERSION = "0.1.0"
+end
