bbd98e3db4b7d361c126a6b9cbd708e0c1f25717
Wendel Scardua <wendelscardua@gmail.com>
2021-10-20 20:20:38 -0300

    Add basic spec

--- spec/golden_section_search_spec.rb
+++ spec/golden_section_search_spec.rb
@@ -3,7 +3,16 @@ RSpec.describe GoldenSectionSearch do
     expect(GoldenSectionSearch::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:function) do
+    proc { |x| (x - 7)**2 }
+  end
+
+  let(:tolerance) { 0.00001 }
+
+  describe '#find_minimum' do
+    it 'returns the input value for which the funcion value is the minimum' do
+      minimum = described_class.find_minimum(function, 0.0, 20.0, tolerance)
+      expect((minimum - 7.0).abs).to be < tolerance
+    end
   end
 end
